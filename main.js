f650ddffe1d83e680a1c5dd5f040209bea7873ca
kapil vaishnav <77320872+kapil5849@users.noreply.github.com>
2022-02-27 21:59:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
